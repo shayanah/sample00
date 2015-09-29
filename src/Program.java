bf4ecbf3c19ffbad2caf98278265c5ab9facdb80
shayanah <shayan.hossi@gmail.com>
2015-09-29 12:16:14 -0400

    Initial commit

---
+++ src/Program.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * Created by shawn on 9/29/2015.
+ */
+public class Program {
+    public static void main(String[] args )
+    {
+        Scanner i = new Scanner(System.in);
+        int a = i.nextInt();
+        System.out.println("Hello world " + a * 2);
+    }
+}
